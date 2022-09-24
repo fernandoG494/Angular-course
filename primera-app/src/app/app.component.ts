7781c6a87abb15d3969890f28854f91a016476d0
Fernando Garcia <fernando_garcia@epam.com>
2022-09-24 12:08:08 -0500

    Practice changes

--- primera-app/src/app/app.component.ts
+++ primera-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primera-app';
+  title = 'Primera App en Angular';
+  saludo = 'Saludos desde Angular';
 }
